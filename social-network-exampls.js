151ba37068447601d50815b9de73b830d443fdb6
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-08-27 10:30:06 -0700

    first commit

--- social-network-exampls.js
+++ social-network-exampls.js
@@ -35,18 +35,61 @@ let users = {
 
   getUserByID: function(userID) {
     // should return a sentence containing all of the info about the user (name age, and bio)
+
+    let answer = "";
+    for (let key in users){
+      if(key === userID){
+        answer = `My name is ${users[key].name}. I'm ${users[key].age} years old. ${users[key].bio}`    
+      }
+    }
+    return answer;
+  },
+
+  generateUid: function() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   },
 
   getUserByName: function(userName) {
      // should return a sentence containing all of the info about the user (name age and bio)
+    let answer = "";
+
+    Object.keys(users).forEach(function(elm){
+      if(users[elm].name === userName){
+        answer = `My name is ${users[elm].name}. I'm ${users[elm].age} years old. ${users[elm].bio}`
+      }
+    });
+    return answer;
   },
 
-  addUser: function(name, price, desc) {
+  addUser: function(name, age, bio, friends) {
     //will need a random id as well when creating a new user. console log the user object to confirm it was added. 
+    let uId = users.generateUid();
+    users[uId] = {
+      id: uId,
+      name: name,
+      age: age,
+      bio: bio,
+      friends: friends
+    }
+    return users;
   },
 
   getFriendsByID: function(userID) {
     // this function will take in a userID, and using this, return a list of all of their friends names. 
+
+    let answer = "";
+    Object.keys(users).forEach(function(elm){
+      if(users[elm].id === userID){
+        answer = users[elm].friends
+      }
+    });
+    return answer;
   }
 
 }
+
+// console.log(users.getUserByID('p04'));
+// console.log(users.getUserByName('Austin Powers'));
+// console.log(users.addUser('Ami Yamanouchi','25','I am a Japanese',['Chiori']));
+console.log(users.getFriendsByID('p03'));
+
